e8ea839c878b564151d50d28c81efc4e691d83f0
Fredincorporation <88601212+Fredincorporation@users.noreply.github.com>
2026-07-21 21:59:22 +0100

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,23 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+    ],
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
